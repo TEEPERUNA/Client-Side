35efb1713681ab11a639adc4ddf290964a1ad720
Erik <e2301760@edu.vamk.fi>
2025-12-01 13:58:15 +0200

    Refactor to MVVM pattern

--- Views/AllNotesPage.xaml.cs
+++ Views/AllNotesPage.xaml.cs
@@ -5,42 +5,16 @@ public partial class AllNotesPage : ContentPage
     public AllNotesPage()
     {
         InitializeComponent();
+        BindingContext = new ViewModels.NotesViewModel();
     }
 
     protected override void OnAppearing()
     {
         base.OnAppearing();
-
-        var notes = new List<Models.Note>();
-
-        var files = Directory.EnumerateFiles(FileSystem.AppDataDirectory, "*.notes.txt");
-        foreach (var filename in files)
+        // Päivitetään lista aina kun sivu tulee näkyviin
+        if (BindingContext is ViewModels.NotesViewModel notesVm)
         {
-            notes.Add(new Models.Note
-            {
-                Filename = filename,
-                Text = File.ReadAllText(filename),
-                Date = File.GetCreationTime(filename)
-            });
-        }
-
-        notesCollection.ItemsSource = notes
-            .OrderBy(d => d.Date)
-            .ToList();
-    }
-
-    private async void Add_Clicked(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync(nameof(NotePage));
-    }
-
-    private async void notesCollection_SelectionChanged(object sender, SelectionChangedEventArgs e)
-    {
-        if (e.CurrentSelection.Count != 0)
-        {
-            var note = (Models.Note)e.CurrentSelection[0];
-            await Shell.Current.GoToAsync($"{nameof(NotePage)}?{nameof(NotePage.ItemId)}={note.Filename}");
-            notesCollection.SelectedItem = null;
+            notesVm.ReloadNotes();
         }
     }
 }
